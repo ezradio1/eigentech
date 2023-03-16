a90481614d5af9d316935b9a7f4cbc5fec2b8bc6
ezradio1 <ezraaudivano@gmail.com>
2023-03-16 16:07:38 +0700

    fix: responsive error state

--- front-end-challenge/src/components/ErrorState/index.tsx
+++ front-end-challenge/src/components/ErrorState/index.tsx
@@ -5,13 +5,8 @@ import FlexLayout from "components/FlexLayout";
 const ErrorState = () => {
   return (
     <FlexLayout width='100%' height='calc(100vh - 120px)' justify='center'>
-      <FlexLayout direction='column' width='100%' justify='center'>
-        <Image
-          preview={false}
-          src={ErrorIllustration}
-          alt='error-state'
-          width={700}
-        />
+      <FlexLayout direction='column' width='700px' justify='center'>
+        <Image preview={false} src={ErrorIllustration} alt='error-state' />
         <h2>An error occurred on the server</h2>
         <p>Please try again later</p>
       </FlexLayout>
